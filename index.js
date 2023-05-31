406d1b674b153224d1d64b2002d83dae6eca9945
YoshiBot <surke@outlook.es>
2023-05-31 17:58:24 -0500

    axios

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const { load } = require("cheerio");
-const fetch = require("node-fetch");
 const cors = require("cors");
+const axios = require("axios");
+
 const app = express();
 const port = process.env.PORT || 3001;
 app.use(cors());
 // respond with "hello world" when a GET request is made to the homepage
 app.get("/", function (req, res) {
-  fetch("https://futbol-libre.org/agenda/")
+  let config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: "https://futbol-libre.org/agenda/",
+    headers: {},
+  };
+
+  axios
+    .request(config)
     .then((textHtml) => {
-      return textHtml.text();
+      return textHtml.data;
     })
     .then((rest) => {
       let $ = load(rest.replaceAll("https://futbol-libre.org", "/pre"));
@@ -23,9 +32,16 @@ app.get("/", function (req, res) {
 app.get("/pre/:subrute", function (req, res) {
   try {
     let route = "https://futbol-libre.org/" + req.params.subrute;
-    fetch(route)
+    let config = {
+      method: "get",
+      maxBodyLength: Infinity,
+      url: route,
+      headers: {},
+    };
+    axios
+      .request(config)
       .then((textHtml) => {
-        return textHtml.text();
+        return textHtml.data;
       })
       .then((rest) => {
         let $ = load(rest.replaceAll("https://futbol-libre.org", ""));
@@ -55,12 +71,14 @@ app.get("/now/:subrute", function (req, res) {
 });
 app.listen(port, () => console.log("servicio en linea en el puerto " + port));
 async function getPage(url) {
-  const requestOptions = {
-    method: "GET",
-    redirect: "follow",
+  let config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: url,
+    headers: {},
   };
-  let solicitud = await fetch(url, requestOptions);
-  let cuerpo = await solicitud.text();
+  let solicitud = await axios.request(config);
+  let cuerpo = await solicitud.data;
   if (solicitud.status === 410) {
     console.log(solicitud.status);
     return await getPage(url);
